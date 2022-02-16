82d16ad0115ef49851dc7c3b92417b452aa6985c
Lawrence Yoon <lyoon@truechoice.io>
2022-02-16 09:45:16 -0800

    feat: render out Logo component

---
+++ components/Header/Logo.js
@@ -0,0 +1,12 @@
+// dependencies
+// local files
+
+/**
+ * @description - returns Logo component
+ * @return {jsx} - the Logo Image to render
+ */
+const Logo = () => {
+  return <div className="logo">LOGO</div>;
+};
+
+export default Logo;

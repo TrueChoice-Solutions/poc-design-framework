7535f2735dd1ce48721da28e5c27f71b63cc518a
Lawrence Yoon <lyoon@truechoice.io>
2022-02-16 11:38:38 -0800

    doc: add copyright & description for Timeline component

--- components/Header/Timeline.js
+++ components/Header/Timeline.js
@@ -1,7 +1,21 @@
+/*
+ * Copyright (c) 2006-present TrueChoice IP Holding Company, Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of TrueChoice IP Holding Company, Inc.
+ * ("Confidential Information").  You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered into with the company.
+ */
+
 // dependencies
 // local files
 import styles from './Timeline.module.css';
 
+/**
+ * @description - returns Timeline component
+ * @param {string} - the text to display
+ * @return {jsx} - the Timeline to render
+ */
 const Timeline = ({ text }) => {
   return (
     <div className={styles.timeline}>

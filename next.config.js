c74e379a1651a974fb890b7c73116991f1efc44e
Lawrence Yoon <lyoon@truechoice.io>
2022-02-16 14:17:05 -0800

    doc: get rid of comment in next config file, answered

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
-// for nextjs, have to add a domain here.. ask ariana about this. If I try commenting out images, throws error
-
 module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['mdbootstrap.com']
-  }
+  reactStrictMode: true
 };

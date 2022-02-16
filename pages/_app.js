1dab73b0b5397bd9b2edf67c9b7b3fa70b83192c
Lawrence Yoon <lyoon@truechoice.io>
2022-02-15 16:05:04 -0800

    doc: add copyright info for app & welcome pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+/*
+ * Copyright (c) 2006-present TrueChoice IP Holding Company, Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of TrueChoice IP Holding Company, Inc.
+ * ("Confidential Information").  You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered into with the company.
+ */
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/welcome.js
+++ pages/welcome.js
@@ -1,5 +1,20 @@
+/*
+ * Copyright (c) 2006-present TrueChoice IP Holding Company, Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of TrueChoice IP Holding Company, Inc.
+ * ("Confidential Information").  You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered into with the company.
+ */
+
 const Welcome = () => {
-  return <h1>Welcome Page!</h1>;
+  return (
+    <main>
+      <h1>
+        Welcome Page!<div></div>
+      </h1>
+    </main>
+  );
 };
 
 export default Welcome;

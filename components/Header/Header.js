bccc7d481063ce9f7a975cb492092ab6ec3be066
Lawrence Yoon <lyoon@truechoice.io>
2022-02-16 09:42:49 -0800

    feat: add Header, now work on creating Logo component

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,3 +1,17 @@
-const Header = () => {};
+// dependencies
+// local files
+import Logo from './Logo/Logo';
+
+/**
+ * @description - returns Header component
+ * @return {jsx} - the Header to render
+ */
+const Header = () => {
+  return (
+    <section className="header">
+      <Logo />
+    </section>
+  );
+};
 
 export default Header;

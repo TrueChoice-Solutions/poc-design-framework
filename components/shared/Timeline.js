9b7a80a7de5369efd1f230b6ce4af9956adbf2f6
Lawrence Yoon <lyoon@truechoice.io>
2022-02-17 08:30:30 -0800

    style: add style & text for Timeline component, currently working on this

--- components/shared/Timeline.js
+++ components/shared/Timeline.js
@@ -7,17 +7,32 @@
  * use it only in accordance with the terms of the license agreement you entered into with the company.
  */
 
+// dependencies
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+// local files
+import styles from './Timeline.module.css';
+
 /**
  * @description - returns Timeline component
  * @param {string} props.text - the text to show which section the user is on
  * @return {jsx} - the Timeline component to render
  */
-
 const Timeline = ({ text }) => {
   return (
-    <div>
-      <div>{text}</div>
-    </div>
+    <Container className={styles.container}>
+      <Row>
+        <Col>
+          <div className={styles.row}>{text}</div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <div>Hello</div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 

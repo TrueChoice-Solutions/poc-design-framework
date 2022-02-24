e5f42a6e258491a9c0a2981e499b16c4825d956d
Lawrence Yoon <lyoon@truechoice.io>
2022-02-24 13:29:17 -0800

    feat: finish Welcome component, next step is to make child Button component

--- components/body/Welcome.js
+++ components/body/Welcome.js
@@ -13,16 +13,36 @@ import styles from './Welcome.module.css';
 
 /**
  * @description - returns Welcome component
+ * @param {string} props.h1Text - h1
  * @return {jsx} - the Welcome component to render
  */
 const Welcome = ({ h1Text, h2Text }) => {
   return (
     <div className={`container-fluid ${styles.outer}`}>
       <div className="container">
-        <div className="row vh-100 min-vh-100 align-items-center">
+        <div className="row vh-100 min-vh-100 align-items-center text-light">
           <div className="col">
             <h1 className="display-2 fw-bolder">{h1Text}</h1>
             <h2 className="fw-bolder">{h2Text}</h2>
+            <hr />
+            <div className="d-flex align-items-center justify-content-between">
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-circle-half me-2"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z" />
+                </svg>
+                It takes less than 5 minutes
+              </div>
+              <button type="button" className="btn btn-danger text-white">
+                Get Started
+              </button>
+            </div>
           </div>
         </div>
       </div>

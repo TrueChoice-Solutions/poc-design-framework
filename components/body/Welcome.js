cb6fdf8983eb42870ddd8c5f809b00c2d47c3750
Lawrence Yoon <lyoon@truechoice.io>
2022-02-28 15:45:21 -0800

    style: update icon size to be 24 width/height

--- components/body/Welcome.js
+++ components/body/Welcome.js
@@ -30,8 +30,8 @@ const Welcome = ({ h1Text, h2Text }) => {
               <div>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
+                  width="24"
+                  height="24"
                   fill="currentColor"
                   className="bi bi-circle-half me-2"
                   viewBox="0 0 16 16"

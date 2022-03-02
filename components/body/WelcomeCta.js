46329c7c10a633c053e3619cf2c02217fa7f4c54
Lawrence Yoon <lyoon@truechoice.io>
2022-03-02 11:07:15 -0800

    refactor: change icon to use react bootstrap icon

--- components/body/WelcomeCta.js
+++ components/body/WelcomeCta.js
@@ -9,6 +9,7 @@
 
 // dependencies
 import Col from 'react-bootstrap/Col';
+import { Clock } from 'react-bootstrap-icons';
 // local files
 import Button from '../shared/Button';
 
@@ -16,21 +17,23 @@ import Button from '../shared/Button';
  * @description - section for icon, time estimation text, and button
  * @return {jsx} - the Welcome Cta to render
  */
+
+//  <svg
+//  xmlns="http://www.w3.org/2000/svg"
+//  width="24"
+//  height="24"
+//  fill="currentColor"
+//  className="bi bi-circle-half"
+//  viewBox="0 0 16 16"
+// >
+//  <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z" />
+// </svg>
 const WelcomeCta = () => {
   return (
     <div className="d-flex align-items-center justify-content-between">
       <Col xs={7} sm className="d-flex align-items-center">
         <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="currentColor"
-            className="bi bi-circle-half"
-            viewBox="0 0 16 16"
-          >
-            <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z" />
-          </svg>
+          <Clock size={24} />
         </div>
         <span className="ms-3">It takes less than 5 minutes</span>
       </Col>

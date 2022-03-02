cd899016b7a05b4eb718ff66f79e09652e4845a7
Lawrence Yoon <lyoon@truechoice.io>
2022-03-02 11:08:03 -0800

    style: delete old svg icon, now using react-bootstrap-icons

--- components/body/WelcomeCta.js
+++ components/body/WelcomeCta.js
@@ -17,17 +17,6 @@ import Button from '../shared/Button';
  * @description - section for icon, time estimation text, and button
  * @return {jsx} - the Welcome Cta to render
  */
-
-//  <svg
-//  xmlns="http://www.w3.org/2000/svg"
-//  width="24"
-//  height="24"
-//  fill="currentColor"
-//  className="bi bi-circle-half"
-//  viewBox="0 0 16 16"
-// >
-//  <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z" />
-// </svg>
 const WelcomeCta = () => {
   return (
     <div className="d-flex align-items-center justify-content-between">

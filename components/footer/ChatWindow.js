70b9e11d29e163fab6dbcb7ce4549f8857792073
Lawrence Yoon <lyoon@truechoice.io>
2022-03-10 12:14:22 -0800

    feat: add user input form on bottom of chat window

--- components/footer/ChatWindow.js
+++ components/footer/ChatWindow.js
@@ -8,7 +8,7 @@
  */
 
 // dependencies
-import { Card } from 'react-bootstrap';
+import { Card, Form, FormLabel, FormControl } from 'react-bootstrap';
 import { X } from 'react-bootstrap-icons';
 // local files
 
@@ -27,16 +27,27 @@ const ChatWindow = ({ cardClassName = 'bg-dark', toggleChatWindowDisplay }) => {
             className="float-end cursor-pointer"
             onClick={toggleChatWindowDisplay}
           />
-          <p className="border bg-secondary bg-gradient w-75 rounded-2 p-3 mb-2">
+          <p className="bg-secondary bg-gradient w-75 rounded-2 p-3 mb-2">
             Please indicate how much you prefer or value each item in relation
             to the others
           </p>
-          <p className="border bg-info bg-gradient text-white w-75 rounded-2 p-3 float-end">
+          <p className="bg-info bg-gradient text-white w-75 rounded-2 p-3 float-end">
             You cannot have the same preference value for all the choices shown.
             Can I help you with something else?
           </p>
         </div>
         <hr />
+        <Form>
+          <FormLabel htmlFor="user-input">
+            Can I help you with something else?
+            <FormControl
+              className="mt-2"
+              id="user-input"
+              type="text"
+              placeholder="Your text here..."
+            />
+          </FormLabel>
+        </Form>
       </Card>
     </div>
   );

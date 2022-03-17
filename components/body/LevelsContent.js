5726b63e5523a169a816b98a2e39c806097651aa
Lawrence Yoon <lyoon@truechoice.io>
2022-03-17 10:46:03 -0700

    refactor: store tooltip & badge inside a div, layout changes for levels

--- components/body/LevelsContent.js
+++ components/body/LevelsContent.js
@@ -24,36 +24,38 @@ const LevelsContent = () => {
     <Container fluid>
       <Container>
         <Row className="vh-100 min-vh-100 align-items-center text-light">
-          <Col className="d-flex align-items-center">
-            <Tooltip
-              contentClassName="bg-dark p-3 text-center"
-              contentAbsoluteStartingPosition="left"
-            >
-              <Image
-                className="mb-3"
-                src="https://media.istockphoto.com/photos/torn-paper-with-word-are-you-covered-picture-id1327364098?s=612x612"
-                alt="random"
-                layout="responsive"
-                width="100"
-                height="100"
+          <Col>
+            <div className="d-flex align-items-center border">
+              <Tooltip
+                contentClassName="bg-dark p-3 text-center"
+                contentAbsoluteStartingPosition="left"
+              >
+                <Image
+                  className="mb-3"
+                  src="https://media.istockphoto.com/photos/torn-paper-with-word-are-you-covered-picture-id1327364098?s=612x612"
+                  alt="random"
+                  layout="responsive"
+                  width="100"
+                  height="100"
+                />
+                <video
+                  className="mb-3"
+                  width="100%"
+                  height="100%"
+                  muted
+                  controls
+                  src="https://static.videezy.com/system/resources/previews/000/038/595/original/alb_lightfx2072_1080p.mp4"
+                ></video>
+                <p className="mb-0">
+                  Benefits that help support achieving a more optimal balance
+                  between work obligations and your other needs / obligations.
+                </p>
+              </Tooltip>
+              <Badge
+                className="bg-primary d-inline-block rounded-pill px-2 ms-3"
+                text="Work / Life Balance"
               />
-              <video
-                className="mb-3"
-                width="100%"
-                height="100%"
-                muted
-                controls
-                src="https://static.videezy.com/system/resources/previews/000/038/595/original/alb_lightfx2072_1080p.mp4"
-              ></video>
-              <p className="mb-0">
-                Benefits that help support achieving a more optimal balance
-                between work obligations and your other needs / obligations.
-              </p>
-            </Tooltip>
-            <Badge
-              className="bg-primary d-inline-block rounded-pill px-2 ms-3"
-              text="Work / Life Balance"
-            />
+            </div>
           </Col>
         </Row>
       </Container>

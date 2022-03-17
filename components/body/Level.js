5d9279b49bbc502b65a834ed3c142721cbd44173
Lawrence Yoon <lyoon@truechoice.io>
2022-03-17 12:16:58 -0700

    doc: add description for Level

--- components/body/Level.js
+++ components/body/Level.js
@@ -7,6 +7,13 @@
  * use it only in accordance with the terms of the license agreement you entered into with the company.
  */
 
+// dependencies
+// local files
+
+/**
+ * @description - returns icon, tooltip, text, and range slider
+ * @return {jsx} - the Level component to render
+ */
 const Level = ({ icon }) => {
   return (
     <div className="border">

8765a0a835368c39c22aaa83bf70300e363dae64
Lawrence Yoon <lyoon@truechoice.io>
2022-03-21 11:46:13 -0700

    style: add comments for future steps

--- components/footer/Navigation.js
+++ components/footer/Navigation.js
@@ -30,3 +30,12 @@ const Navigation = () => {
 };
 
 export default Navigation;
+
+/* pass Button text as props (for both? If user wants to do 'back' text)
+ * add as params, update description
+ * set default prop values to blank, just in case user only wants icons
+ * set up on click handlers for each button
+ * console log content for now
+ * add description for each event handler function
+ * use next router to goBack or for next, set up => WelcomePage for now
+ */

ed715cfb3820bacc51f60753292413de606bdd2a
Lawrence Yoon <lyoon@truechoice.io>
2022-03-23 08:23:52 -0700

    doc: add copyright info

--- components/body/LevelInfo.js
+++ components/body/LevelInfo.js
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2006-present TrueChoice IP Holding Company, Inc.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of TrueChoice IP Holding Company, Inc.
+ * ("Confidential Information").  You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered into with the company.
+ */
+
 const LevelInfo = () => {
   return (
     <div>
@@ -8,7 +17,7 @@ const LevelInfo = () => {
 
 export default LevelInfo;
 
-/* add copyright info at top
+/*
  * add description info for LevelInfo func
  * add icon, tooltip, text here. (maybe use children..?)
  * update description with props
